5c14c55841681bdaaa21804aa99b09feb7df61bd
github username <github email address>
2018-12-05 09:19:10 -0500

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,42 @@
-class Deck
-
-end
+require 'pry'
 
 class Card
+  attr_reader :rank, :suit
+  @rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  @suit = ["Hearts" , "Clubs", "Diamonds", "Spades"]
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
 
 end
+
+class Deck
+  attr_accessor :cards
+  @@all = []
+
+  def initialize
+    @cards = cards
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.choose_card
+    @@all.each do |card|
+      card
+      binding.pry
+    end
+  end
+
+end
+
+
+deck = Deck.new
+card = Card.new("A", "Clubs")
+
+Deck.choose_card
+#selects random card from the deck and removes it.
